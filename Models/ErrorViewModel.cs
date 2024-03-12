b17dccd1462c134066cfb05bec37d8ec7cd06bd8
Vu Tien Thanh <vutienthanh25092003@gmail.com>
2024-03-11 21:14:45 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Enterprise_Web_Development_1640.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
